3a2711b4fd3ff0d5ff387f2442e5ff3d15737454
t968120 <casper.tollefsen@telenor.no>
2022-10-21 10:11:58 +0200

    Add Header component

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="Header">
+        <h1 class="Header__brand-name">Vue Masterclass</h1>
+    </header>
+</template>
+
+<style>
+.Header {
+    border-bottom: 2px solid black;
+}
+.Header__brand-name {
+    font-size: 1.4rem;
+    line-height: 1;
+    margin: 0;
+    padding: 1rem;
+}
+</style>
