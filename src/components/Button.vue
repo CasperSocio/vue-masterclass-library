fbfd340513a1469164dbaada6f6750a210c157b7
t968120 <casper.tollefsen@telenor.no>
2022-10-21 09:49:20 +0200

    Add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+    label: string
+    shape?: 'pill' | 'square'
+    type?: 'button' | 'reset' | 'submit'
+    variant?: 'primary' | 'secondary'
+}>(), {
+    shape: 'pill',
+    type: 'button',
+    variant: 'secondary'
+})
+
+const classes = [
+    'Button',
+    `Button--${ props.shape }`,
+    `Button--${ props.variant }`,
+].join(' ')
+</script>
+
+<template>
+    <button :class="classes" :type="type">
+        {{ label }}
+    </button>
+</template>
+
+<style>
+.Button {
+    --button-bg: slategray;
+    --button-border-radius: 5rem;
+
+    background-color: var(--button-bg);
+    border: 1px solid transparent;
+    border-radius: var(--button-border-radius);
+    color: white;
+    cursor: pointer;
+    font-family: inherit;
+    font-size: 1em;
+    font-weight: 500;
+    padding: 0.6em 1.2em;
+    transition: border-color 0.25s;
+}
+.Button:hover {
+    border-color: #646cff;
+}
+.Button:focus,
+.Button:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+}
+
+.Button--primary {
+    --button-bg: slateblue;
+}
+
+.Button--square {
+    --button-border-radius: 4px;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,42 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import Button from './Button.vue'
 
-defineProps<{ msg: string }>()
+const props = defineProps<{
+  msg: string
+}>()
 
 const count = ref(0)
+
+const handleDecrement = () => {
+  if (count.value > 0) {
+    count.value--
+  }
+}
+
+const handleIncrement = () => {
+  count.value++
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>Count is {{ count }}</p>
+
+    <!-- Increment button -->
+    <Button
+      label="increment"
+      variant="primary"
+      @click="handleIncrement" />
+
+    <!-- Decrement button -->
+    <Button
+      label="decrement"
+      variant="secondary"
+      @click="handleDecrement" />
+
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
