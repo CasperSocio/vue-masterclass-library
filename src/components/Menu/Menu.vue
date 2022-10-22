4a637462bd3acabaab7ba14458947ade0acbec54
CasperMobit <casper.tollefsen@mobit.no>
2022-10-22 16:28:59 +0200

    Add Menu component

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import MenuItem from './MenuItem.vue'
+
+type Action = {
+  /** The text to display on the menu item */
+  label: string
+}
+
+defineProps<{
+  /** Menu content/items */
+  actions: Action[]
+}>()
+</script>
+
+<template>
+  <menu class="Menu">
+    <MenuItem
+      v-for="action in actions">
+      {{ action.label }}
+    </MenuItem>
+  </menu>
+</template>
+
+<style>
+.Menu {
+  background-color: #dedede;
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+  list-style: none;
+  padding: 2px;
+  width: fit-content;
+}
+</style>
---
+++ src/components/Menu/MenuItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li class="MenuItem">
+    <button
+      class="MenuItem__button"
+      type="button">
+      <slot />
+    </button>
+  </li>
+</template>
+
+<style>
+.MenuItem__button {
+  background-color: #fff;
+  border: 2px solid #fff;
+  cursor: pointer;
+  font-size: 1em;
+  font-weight: 400;
+  line-height: 1;
+  padding: .6em 1.2em;
+  text-align: left;
+  width: 100%;
+}
+
+.MenuItem__button:hover {
+  border-color: slateblue;
+}
+</style>
