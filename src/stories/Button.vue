e68a4b8bef78e62aa549e02fca54701a2efea8f4
Casper Tollefsen <casper.tollefsen@telenor.no>
2022-10-24 15:28:49 +0200

    Convert Button to SCF

--- src/stories/Button.vue
+++ src/stories/Button.vue
@@ -1,52 +1,64 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = withDefaults(defineProps<{
+  label: string
+  primary?: boolean
+  size?: 'small' | 'medium' | 'large'
+}>(), {
+  primary: false,
+  size: 'medium'
+})
+
+const emit = defineEmits(['click'])
+
+const classes = computed(() => ({
+  'Button': true,
+  'Button--primary': props.primary,
+  'Button--secondary': !props.primary,
+  [`Button--${props.size}`]: true,
+}))
+
+</script>
+
 <template>
-  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
+  <button
+    :class="classes"
+    type="button"
+    @click="emit('click')">
+    {{ label }}
+  </button>
 </template>
 
-<script>
-import './button.css';
-import { reactive, computed } from 'vue';
-
-export default {
-  name: 'my-button',
-
-  props: {
-    label: {
-      type: String,
-      required: true,
-    },
-    primary: {
-      type: Boolean,
-      default: false,
-    },
-    size: {
-      type: String,
-      validator: function (value) {
-        return ['small', 'medium', 'large'].indexOf(value) !== -1;
-      },
-    },
-    backgroundColor: {
-      type: String,
-    },
-  },
-
-  emits: ['click'],
-
-  setup(props, { emit }) {
-    props = reactive(props);
-    return {
-      classes: computed(() => ({
-        'storybook-button': true,
-        'storybook-button--primary': props.primary,
-        'storybook-button--secondary': !props.primary,
-        [`storybook-button--${props.size || 'medium'}`]: true,
-      })),
-      style: computed(() => ({
-        backgroundColor: props.backgroundColor,
-      })),
-      onClick() {
-        emit('click');
-      }
-    }
-  },
-};
-</script>
+<style>
+.Button {
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+}
+.Button--primary {
+  color: white;
+  background-color: #1ea7fd;
+}
+.Button--secondary {
+  color: #333;
+  background-color: transparent;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+}
+.Button--small {
+  font-size: 12px;
+  padding: 10px 16px;
+}
+.Button--medium {
+  font-size: 14px;
+  padding: 11px 20px;
+}
+.Button--large {
+  font-size: 16px;
+  padding: 12px 24px;
+}
+</style>
