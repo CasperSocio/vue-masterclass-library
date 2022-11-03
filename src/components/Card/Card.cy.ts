df0311413b6e33c61d42271672a053c39d27c3e8
CasperSocio <casper.tollefsen@outlook.com>
2022-11-03 15:16:22 +0100

    Fix test failing

--- src/components/Card/Card.cy.ts
+++ src/components/Card/Card.cy.ts
@@ -3,7 +3,7 @@ import Card from './Card.vue'
 describe('Card', () => {
 	it('has the correct defaults', () => {
 		cy.mount(Card)
-		cy.get('Card')
+		cy.get('.Card')
 			.should('have.class', 'Card')
 			.should('not.have.attr', 'disabled')
 	})
