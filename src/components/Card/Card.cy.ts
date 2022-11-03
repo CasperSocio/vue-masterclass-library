41d5790e3c1dc6713315c66f8276749d04748131
CasperSocio <casper.tollefsen@outlook.com>
2022-11-03 15:41:32 +0100

    Update tests

--- src/components/Card/Card.cy.ts
+++ src/components/Card/Card.cy.ts
@@ -7,4 +7,24 @@ describe('Card', () => {
 			.should('have.class', 'Card')
 			.should('not.have.attr', 'disabled')
 	})
+	it('renders props and slots', () => {
+		cy.mount(Card, {
+			props: {
+				subtitle: 'Habanero',
+				title: 'Hello!',
+			},
+			slots: {
+				default: () => 'Primary content',
+				footer: () => 'I am the footer',
+			},
+		})
+		cy.get('h3')
+			.should('have.text', 'Hello!')
+		cy.get('h4')
+			.should('have.text', 'Habanero')
+		cy.get('.Card__content')
+			.should('have.text', 'Primary content')
+		cy.get('footer')
+			.should('have.text', 'I am the footer')
+	})
 })
