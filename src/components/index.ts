e64237c05c7d5c92210fbd96478932a5802463ad
CasperSocio <casper.tollefsen@outlook.com>
2023-06-24 13:33:31 +0200

    #28 Move component exports

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Only export components to be included in the library.
+ * Helper components should not be included.
+ */
+
+export { default as Button } from './Button/Button.vue'
+export { default as Card } from './Card/Card.vue'
+export { default as Header } from './Header/Header.vue'
+export { default as Icon } from './Icon/Icon.vue'
+export { default as Menu } from './Menu/Menu.vue'
+export { default as Tip } from './Tip/Tip.vue'
